661d158504c360a5a27dd64593fe64782d03eec2
Annhilati <antbe.artis@web.de>
2026-03-24 14:24:01 +0100

    Sync

--- build_static.py
+++ build_static.py
@@ -3,7 +3,8 @@ from main import app, vp, cfg
 
 with app.app_context():
     data = vp.fetch()
-    html = render_template("main.html", vp=data, cfg=cfg)
+    with app.test_request_context():
+        html = render_template("main.html", vp=data, cfg=cfg)
 
     with open("build/index.html", "w", encoding="utf-8") as f:
         f.write(html)
